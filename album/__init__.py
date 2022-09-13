630dbeedce435925006a5bfbae4a6b2be7c920c2
sanju <sanjusk08gmail.com>
2022-09-13 13:43:29 +0530

    - connected flask app with sqlite database "database.db" - added a command line command  "init-db" to drop and initiate database. - command line support is added by click command line parser (already a dependency of flask).

--- album/__init__.py
+++ album/__init__.py
@@ -1,5 +1,22 @@
+import click
 from flask import Flask 
+from flask.cli import with_appcontext
+
+from .database import db 
+
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.db'
+    db.init_app(app)
+    app.cli.add_command(init_db)
     return app
+
+
+@click.command("init-db")
+@with_appcontext
+def init_db():
+    db.drop_all()
+    db.create_all()
+    click.echo("Initialized databases")
+
     
