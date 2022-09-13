be98c2aea68d70814a210ec533b5b910af3c561c
AMALNR98 <amalnr123@gmail.com>
2022-09-13 15:40:09 +0530

    Added relationships

--- album/database.py
+++ album/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import func
 
 db = SQLAlchemy()
 
-class Users(db.Model):
+class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
     fname = db.Column(db.String, nullable = False)
@@ -11,27 +11,29 @@ class Users(db.Model):
     email = db.Column(db.String, nullable = False, unique = True)
     dob = db.Column(db.Date, nullable = False)
     password = db.Column(db.String, nullable = False)
+    albums = db.relationship("Album")
 
 
     def __repr__(self) -> str:
         return f"User({self.fname} {self.lname})"
 
 
-class Albums(db.Model):
+class Album(db.Model):
     __tablename__ = "albums"
     id = db.Column(db.Integer, primary_key =True)
     name = db.Column(db.String, nullable = False)
     create_date = db.Column(db.DateTime, default = func.now(), nullable = False)
     last_opened = db.Column(db.DateTime, onupdate = func.now())
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    photos = db.relationship("Photo")
     __table_args__ = (db.UniqueConstraint('user_id', 'name', name='_user_id_name_uc'),)
-    
+
     def __repr__(self) -> str:
-        return f"Albums({self.name})"
+        return f"Album({self.name})"
 
 
 
-class Photos(db.Model):
+class Photo(db.Model):
     __tablename__ = "photos"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Integer, nullable = False)
@@ -39,14 +41,15 @@ class Photos(db.Model):
     last_opened = db.Column(db.DateTime, onupdate = func.now())
     uploaded_date = db.Column(db.DateTime, default = func.now(), nullable = False)
     album_id = db.Column(db.Integer, db.ForeignKey("albums.id"), nullable = False)       
+    photos = db.relationship("Comment")
     __table_args__ = (db.UniqueConstraint('album_id', 'name', name='_album_id_name_uc'),)
 
     def __repr__(self) -> str:
-        return f"Photos({self.name})"
+        return f"Photo({self.name})"
 
 
-class Comments(db.Model):
-    __tablename__ = " comments"
+class Comment(db.Model):
+    __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key = True)
     display_name = db.Column(db.Integer, nullable = False)
     photo_id = db.Column(db.Integer, primary_key = True)
@@ -54,6 +57,6 @@ class Comments(db.Model):
     comment = db.Column(db.String, nullable = False)
 
     def __repr__(self) -> str:
-        return f"Comments({self.name})"
+        return f"Comment({self.name})"
 
 
