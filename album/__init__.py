0fe6212ffb808e32d4f1159609e44a4eaacffe7f
sanju <sanjusk08gmail.com>
2022-09-15 15:57:42 +0530

    - implemented a temporary upload view using flask-reupload - added flask-reupload configuration codes in __init__.py

--- album/__init__.py
+++ album/__init__.py
@@ -2,9 +2,10 @@ import click
 from flask import Flask 
 from flask.cli import with_appcontext
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import configure_uploads 
 
 from .auth import auth_bp, login_manager
-from .album_app import album_bp
+from .album_app import album_bp, uploaded_images
 from .database import db
 
 
@@ -12,10 +13,12 @@ from .database import db
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.db'
+    app.config["UPLOADED_PHOTOS_DEST"] = "static/"
     # app.config['SQLALCHEMY_ECHO'] = True
     app.secret_key = 'dev'
     db.init_app(app)
     login_manager.init_app(app)
+    configure_uploads(app, uploaded_images)
     app.register_blueprint(auth_bp)
     app.register_blueprint(album_bp)
     app.cli.add_command(init_db)
--- album/album_app.py
+++ album/album_app.py
@@ -1,12 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, request
 from flask_login import current_user
+from flask_uploads import UploadSet, IMAGES
 
 album_bp = Blueprint('album', '__name__')
+uploaded_images = UploadSet('photos', IMAGES)
 
 @album_bp.route('/')
 def index():
     return render_template('home.html', user=current_user)
 
-
+@album_bp.route('/upload', methods=['POST', 'GET'])
+def test_upload():
+    current_album = 3
+    if request.method == 'POST':
+        print(request.files['photo'])
+        file_path = uploaded_images.save(request.files['photo'], f"{current_user.id}/{current_album}")
+        
+        return render_template('upload_form.html')
+    return render_template('upload_form.html')
 
 
