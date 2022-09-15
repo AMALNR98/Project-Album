5c1d541931fbd22253aef0f86d5e53b10b7f1af0
AMALNR98 <amalnr123@gmail.com>
2022-09-15 15:49:03 +0530

    implimented home view

--- album/album_app.py
+++ album/album_app.py
@@ -1,11 +1,20 @@
+from unicodedata import name
 from flask import Blueprint, render_template
 from flask_login import current_user
+
+from album.database import Album
 
 album_bp = Blueprint('album', '__name__')
 
 @album_bp.route('/')
 def index():
-    return render_template('home.html', user=current_user)
+    list_of_albums = current_user.albums 
+
+    print(list_of_albums)
+    print(current_user.id)
+    return render_template('home.html', user=current_user, albums = list_of_albums)
+
+
 
 
 
