a9f5ba9097b0e07c02bfcd3e371caf0fec0ce86d
AMALNR98 <amalnr123@gmail.com>
2022-09-15 17:05:25 +0530

    implemented album view

--- album/album_app.py
+++ album/album_app.py
@@ -1,9 +1,10 @@
+from unicodedata import name
 from flask import Blueprint, render_template, flash, request
 
 from flask_login import current_user
 from flask_uploads import UploadSet, IMAGES
 
-from album.database import Album
+from album.database import Album, Photo
 
 album_bp = Blueprint('album', '__name__')
 uploaded_images = UploadSet('photos', IMAGES)
@@ -28,4 +29,7 @@ def test_upload():
         return render_template('upload_form.html')
     return render_template('upload_form.html')
 
-
+@album_bp.route('/{album_name}')
+def album():
+    list_of_photos = Photo.query.filter_by(album_name = name).all()
+    return render_template('photos.html',user=current_user, photos = list_of_photos )
