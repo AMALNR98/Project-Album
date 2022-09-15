bea7218cce367c660055d001cf2526cb2220d9f0
sanju <sanjusk08gmail.com>
2022-09-15 17:01:31 +0530

    added description field to album table. added add_album view

--- album/album_app.py
+++ album/album_app.py
@@ -2,8 +2,9 @@ from flask import Blueprint, render_template, flash, request
 
 from flask_login import current_user
 from flask_uploads import UploadSet, IMAGES
+from flask_login import login_required
 
-from album.database import Album
+from album.database import Album, db
 
 album_bp = Blueprint('album', '__name__')
 uploaded_images = UploadSet('photos', IMAGES)
@@ -20,7 +21,7 @@ def index():
 
 @album_bp.route('/upload', methods=['POST', 'GET'])
 def test_upload():
-    current_album = 3
+    current_album = 4
     if request.method == 'POST':
         print(request.files['photo'])
         file_path = uploaded_images.save(request.files['photo'], f"{current_user.id}/{current_album}")
@@ -29,3 +30,15 @@ def test_upload():
     return render_template('upload_form.html')
 
 
+
+@album_bp.route('/add_album', methods=['POST', 'GET'])
+@login_required
+def add_album():
+    if request.method == 'POST':
+        album_name = request.form.get('name')
+        description = request.form.get('description')
+        album = Album(name=album_name, description=description, user_id=current_user.id)
+        db.session.add(album)
+        db.session.commit()
+        flash("album added successfully")
+    return render_template('add_album.html', user=current_user)
