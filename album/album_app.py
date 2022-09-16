54d758abb9d02e0f1374f13aee5cd30739e9e127
AMALNR98 <amalnr123@gmail.com>
2022-09-16 18:21:47 +0530

    implemented view_photo in album_app.py

--- album/album_app.py
+++ album/album_app.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from unicodedata import name
 from flask import Blueprint, render_template, flash, request
 
 from flask import Blueprint, render_template, flash, request, redirect
@@ -15,8 +16,8 @@ uploaded_images = UploadSet('photos', IMAGES)
 @album_bp.route('/')
 def index():
     list_of_albums = current_user.albums 
-    print(list_of_albums)
-    print(current_user.id)
+    # print(list_of_albums)
+    # print(current_user.id)
     return render_template('home.html', user=current_user, albums = list_of_albums)
 
 
@@ -25,7 +26,7 @@ def index():
 def test_upload():
     current_album = 4
     if request.method == 'POST':
-        print(request.files['photo'])
+        # print(request.files['photo'])
         file_path = uploaded_images.save(request.files['photo'], f"{current_user.id}/{current_album}")
         
         return render_template('upload_form.html')
@@ -53,9 +54,9 @@ def album(user_id,album_name):
                 photos = album.photos
                 path = 'users/'  + str(current_user.id) + '/' + str(album.name)
             else:
-                photos = []
                 flash ('no such album')
                 path = None
+                return '404'
             return render_template('photos.html', user=current_user, photos=photos, path=path)
         else:
             album = current_user.album.filter_by(name=album_name).first()
@@ -105,17 +106,40 @@ def add_photo(album_name):
 
     return render_template('add_photo.html', user=current_user)
 
-@album_bp.route('/<int:user_id>/<string:album_name>/<string:photo_name>')
-@login_required
-def photo_view(album_name,photo_name):
-    if current_user.is_authenticated:
-        album = current_user.albums.filter_by(name=album_name).first()
-        print(album)
-        photo = album.Photo(name= name )
-        return render_template('photo.html', user = current_user )
+# @album_bp.route('/<int:user_id>/<string:album_name>/<string:photo_name>')
+# @login_required
+# def view_photo(user_id, album_name,photo_name):
+#     if current_user.is_authenticated:
+#         album = current_user.albums.filter_by(name=album_name).first()
+#         print(album.id)
+#         photo = Photo.filter_by(name= photo_name).first()
+#         print(photo.id)
+#         return render_template('photo.html', user = current_user )
 
-    else:
-        flash("photo is private")        
-        return render_template('photo.html')
+#     else:
+#         flash("photo is private")        
+#         return render_template('photo.html')
     
+@album_bp.route('/<int:user_id>/<string:album_name>/photos/<string:photo_name>')
+def view_photo(user_id,album_name,photo_name):
+        photo = None
+        print("user id =",user_id)
+        # print(current_user)
+        print("current_user.id",current_user.id)
+        if current_user.is_authenticated and current_user.id == user_id :
+            album = current_user.albums.filter_by(name=album_name).first()
+            print(current_user)
+            print(album.id)            
+            photo = album.photos.filter_by(name = photo_name).first()
+            path = 'users/'  + str(current_user.id) + '/' + str(album.name) 
+            print('path =',path)
+            print("photo.name=",photo.name)
+            print("photo id =",photo.id)
+            return render_template('photo.html',photo = photo,user = current_user, path = path)
+        else:
+            flash("photo is private")        
+              
+            return "text"
+            # return render_template('photo.html', user = current_user , photo_name=photo.name)
 
+        
