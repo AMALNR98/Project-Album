175eb6112524b648f3a2cd3569aae37c9358786a
sanju <sanjusk08gmail.com>
2022-09-16 19:03:57 +0530

    implemented album form

--- album/forms.py
+++ album/forms.py
@@ -1,7 +1,8 @@
 from sqlite3 import Date
-from tkinter.tix import Form
-from wtforms import  Form, BooleanField, StringField, PasswordField, validators, DateField
+from wtforms import  Form,  BooleanField, StringField, PasswordField, validators, DateField, SelectField, FileField
 from wtforms.validators import Email, Length, DataRequired, EqualTo
+from  flask_wtf.file import FileRequired, FileAllowed
+ 
 
 class RegistrationForm(Form):
     fname = StringField( [Length(min=4, max=25,), DataRequired()])
@@ -23,4 +24,8 @@ class LoginForm(Form):
         Length(min=6, max=35),])
 
 
+class AlbumForm(Form):
+    name = StringField('name', [Length(min=1, max=50), DataRequired()])
+    description = StringField('description', [Length(min=0, max=50 )])
+    status = SelectField('status', choices=['Private', 'Public'])
 
