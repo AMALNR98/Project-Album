3ae6243741f7dcc59dfdd57960ab683ff2ed591b
AMALNR98 <amalnr123@gmail.com>
2022-09-17 11:32:50 +0530

    refactored album_app.py

--- album/album_app.py
+++ album/album_app.py
@@ -1,6 +1,4 @@
-import logging
 import os
-from unicodedata import name
 from flask import Blueprint, render_template, flash, request
 
 from flask import Blueprint, render_template, flash, request, redirect
@@ -15,10 +13,8 @@ uploaded_images = UploadSet('photos', IMAGES)
 
 @album_bp.route('/')
 def index():
-    list_of_albums = current_user.albums 
-    # print(list_of_albums)
-    # print(current_user.id)
-    return render_template('home.html', user=current_user, albums = list_of_albums)
+    albums = current_user.albums 
+    return render_template('home.html', user=current_user, albums = albums)
 
 
 
@@ -31,18 +27,6 @@ def test_upload():
         
         return render_template('upload_form.html')
     return render_template('upload_form.html')
-
-# @album_bp.route('/<string:album_name>')
-# def album(album_name):
-#     # list_of_photos = Photo.query.filter_by(name = album_name).all()
-#     album = current_user.albums.filter_by(name=album_name).first()
-#     if album == None:
-#         flash("Nothing to diplay")
-#         photos = []
-#     else:
-#         photos = album.photos
-#     path = 'users/'  + str(current_user.id) + '/' + str(album.name)
-#     return render_template('photos.html',user=current_user, photos = photos,  path=path)
 
 
 @album_bp.route('/<int:user_id>/<string:album_name>')
@@ -106,19 +90,7 @@ def add_photo(album_name):
 
     return render_template('add_photo.html', user=current_user)
 
-# @album_bp.route('/<int:user_id>/<string:album_name>/<string:photo_name>')
-# @login_required
-# def view_photo(user_id, album_name,photo_name):
-#     if current_user.is_authenticated:
-#         album = current_user.albums.filter_by(name=album_name).first()
-#         print(album.id)
-#         photo = Photo.filter_by(name= photo_name).first()
-#         print(photo.id)
-#         return render_template('photo.html', user = current_user )
 
-#     else:
-#         flash("photo is private")        
-#         return render_template('photo.html')
     
 @album_bp.route('/<int:user_id>/<string:album_name>/photos/<string:photo_name>')
 def view_photo(user_id,album_name,photo_name):
@@ -155,8 +127,3 @@ def view_photo(user_id,album_name,photo_name):
         
                             
 
-              
-            # return "text"
-            # return render_template('photo.html', user = current_user , photo_name=photo.name)
-
-        
