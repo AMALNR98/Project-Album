c6fc0ba2ce5e502f03d973a9f21db7cb6f1a1cc6
sanju <sanjusk08gmail.com>
2022-09-19 09:48:17 +0530

    refactored forms.py

--- album/forms.py
+++ album/forms.py
@@ -1,38 +1,38 @@
 
 
 
-from wtforms import  Form,  BooleanField, StringField, PasswordField, validators, DateField, SelectField, FileField
+from wtforms import  Form,  BooleanField, StringField, PasswordField, validators, DateField, SelectField, FileField, TextAreaField
 from wtforms.validators import Email, Length, DataRequired, EqualTo
 from  flask_wtf.file import FileRequired, FileAllowed
+from flask_wtf import FlaskForm
  
 
-class RegistrationForm(Form):
-    fname = StringField( [Length(min=4, max=25,), DataRequired()])
+class RegistrationForm(FlaskForm):
+    fname = StringField( [Length(min=4, max=25,), DataRequired('field required')])
     lname = StringField( [Length(min=0, max=25), DataRequired()])
-    dob = DateField( [DataRequired()])
-    email = StringField( [Length(min=6, max=120), DataRequired(), Email()])
+    dob = DateField( [DataRequired('field is required')])
+    email = StringField( [Length(min=6, max=120), DataRequired('feild required'), Email('invalid email')])
     password = PasswordField( [
         DataRequired(),
-        EqualTo('confirm', message='Passwords must match'),
         Length(min=6, max=35)
     ])
-    confirm = PasswordField( [DataRequired()])
+    confirm = PasswordField('confirm', [DataRequired('field required'), EqualTo('password')])
 
 
-class LoginForm(Form):
-    email = StringField('Email Address', [Length(min=6, max=120), DataRequired(), Email()])
-    password = PasswordField('New Password', [
+class LoginForm(FlaskForm):
+    email = StringField('Email Address', [Length(min=6, max=120), DataRequired(), Email('email invalid')])
+    password = PasswordField('Password', [
         DataRequired(),
         Length(min=6, max=35),])
 
 
-class PhotoForm(Form):
-    description = StringField([Length(min=2, max=100), DataRequired()])
+class PhotoForm(FlaskForm):
+    description = TextAreaField([Length(min=2, max=100), DataRequired()])
     photo = FileField(validators=[FileRequired(),FileAllowed(['png','jpeg','jpg'],)])
     status = SelectField(choices=[('private'), ('public')])
 
-class AlbumForm(Form):
+class AlbumForm(FlaskForm):
     name = StringField('name', [Length(min=1, max=50), DataRequired()])
-    description = StringField('description', [Length(min=0, max=50 )])
+    description = TextAreaField('description', [Length(min=0, max=50 )])
     status = SelectField('status', choices=['Private', 'Public'])
 
