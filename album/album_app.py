964ffc6069067f7061fba0d11a62bdea5fcf135d
sanju <sanjusk08gmail.com>
2022-09-25 13:25:26 +0530

    refactored views to render proper 401 and 404 templates

--- album/album_app.py
+++ album/album_app.py
@@ -1,6 +1,6 @@
 import os
-from flask import Blueprint, render_template, flash, request, redirect, url_for, jsonify
-from flask_login import current_user, login_required
+from flask import Blueprint, render_template, flash, request, redirect, url_for, jsonify, current_app
+from flask_login import current_user, login_required, login_manager
 from flask_uploads import UploadSet, IMAGES
 
 from album.database import Album, Photo, db, User, Comment
@@ -19,14 +19,12 @@ def index():
     else:
         return render_template("home.html", user=current_user, albums=None, form=form)
 
-
        
 @album_bp.route('/<int:user_id>/albums', methods=['POST',])
 @login_required
 def add_album(user_id):
     form = AlbumForm(request.form)
     if form.validate():
-        print(form.status)
         if form.status.data == "Public":
             public = True
         else:
@@ -39,7 +37,6 @@ def add_album(user_id):
         )
         db.session.add(album)
         db.session.commit()
-        print("album added successfully")
         flash("album added successfully")
     return redirect(url_for("album.index"))
 
@@ -63,16 +60,13 @@ def view_albums(user_id):
             form=form,
 
         )
-
     else:
-        user = User.query.get(user_id)
-        if user:
-            albums = user.albums.filter_by(public=True).all()
-            return render_template(
-                "albums.html", current_user=current_user, user=user, albums=albums
-            )
-        else:
-            return render_template("404.html")
+        user = User.query.get_or_404(user_id)
+        albums = user.albums.filter_by(public=True).all()
+        return render_template(
+            "albums.html", current_user=current_user, user=user, albums=albums
+        )
+
 
 @album_bp.route('/<int:user_id>/albums/<string:album_name>', methods=['DELETE',])
 @login_required
@@ -84,125 +78,95 @@ def delete_album(user_id,album_name):
         return '', 204
 
     else :
-        return 'not authorized'
+        return current_app.login_manager.unauthorized() 
 
 @album_bp.route('/<int:user_id>/albums/<string:album_name>', methods=['GET' ])
 def view_album(user_id,album_name):
     form = PhotoForm(request.form )
     if current_user.is_authenticated and current_user.id == user_id:
-        album = current_user.albums.filter_by(name=album_name).first()
-        if album:
-            photos = album.photos
-            path = 'users/'  + str(current_user.id) + '/' + str(album.name)
-            print(path)
-            return render_template('photos.html', current_user=current_user, user=current_user, photos=photos, path=path, form = form,album_name = album_name, album=album)
-        else:
-            return render_template("404.html"), 404
+        album = current_user.albums.filter_by(name=album_name).first_or_404()
+        photos = album.photos
+        path = 'users/'  + str(current_user.id) + '/' + str(album.name)
+        return render_template('photos.html', current_user=current_user, user=current_user, photos=photos, path=path, form = form,album_name = album_name, album=album)
 
     else:
         user = User.query.get_or_404(user_id)
-        album = user.albums.filter_by(name=album_name).first()
-        if album:
-            if album.public:
-                photos = album.photos.filter_by(public=True).all()
-                path = 'users/'  + str(user.id) + '/' + str(album.name)
-                print(album_name)
-                return render_template('photos.html', current_user=current_user, user=user, photos=photos, path=path, form = form,album_name = album_name, album=album)
-            else:
-                return 'private', 401
+        album = user.albums.filter_by(name=album_name).first_or_404()
+        if album.public:
+            photos = album.photos.filter_by(public=True).all()
+            path = 'users/'  + str(user.id) + '/' + str(album.name)
+            return render_template('photos.html', current_user=current_user, user=user, photos=photos, path=path, form = form,album_name = album_name, album=album)
         else:
-            return render_template("404.html"), 404
+            return current_app.login_manager.unauthorized()
         
 
 @album_bp.route('/<int:user_id>/albums/<string:album_name>', methods=['POST'])
 @login_required
 def add_photo(user_id, album_name):
-    album = current_user.albums.filter_by(name=album_name).first()
-    if album:
-        file_path = uploaded_images.save(
-            request.files["photo"], f"{current_user.id}/{album_name}"
-        )
-        file_name = os.path.basename(file_path)
-        size = 0
-        if album.public == True:
-            photo = Photo(
-                name=file_name,
-                size=size,
-                album_id=album.id,
-                description=request.form.get("description"),
-                public = True
-            )
-        else:
-            photo = Photo(
-                name=file_name,
-                size=size,
-                album_id=album.id,
-                description=request.form.get("description"),
-                public = False
-            )
-
-        db.session.add(photo)
-        db.session.commit()
-        flash(f"{file_name} added successfully")
-        return redirect(
-            url_for("album.view_album", user_id=user_id, album_name=album_name)
+    album = current_user.albums.filter_by(name=album_name).first_or_404()
+    file_path = uploaded_images.save(
+        request.files["photo"], f"{current_user.id}/{album_name}"
+    )
+    file_name = os.path.basename(file_path)
+    size = 0
+    if album.public == True:
+        photo = Photo(
+            name=file_name,
+            size=size,
+            album_id=album.id,
+            description=request.form.get("description"),
+            public = True
         )
     else:
-        return render_template("404.html"), 404
+        photo = Photo(
+            name=file_name,
+            size=size,
+            album_id=album.id,
+            description=request.form.get("description"),
+            public = False
+        )
 
+    db.session.add(photo)
+    db.session.commit()
+    flash(f"{file_name} added successfully")
+    return redirect(
+        url_for("album.view_album", user_id=user_id, album_name=album_name)
+    )
 
+    
 @album_bp.route('/<int:user_id>/albums/<string:album_name>/<string:photo_name>')
 def view_photo(user_id,album_name,photo_name):
     photo = None
     if current_user.is_authenticated and current_user.id == user_id :
-        album = current_user.albums.filter_by(name=album_name).first()
-        if album:           
-            photo = album.photos.filter_by(name = photo_name, album_id=album.id).first()
-            if photo:
-                path = 'users/'  + str(current_user.id) + '/' + str(album.name) 
-                   
-                return render_template('photo.html',photo = photo, current_user=current_user, user = current_user, path = path, album_name=album_name)
-            else:
-                return "photo not found"
-        else:
-            return '404', 404
-            
+        album = current_user.albums.filter_by(name=album_name).first_or_404()
+        photo = album.photos.filter_by(name = photo_name, album_id=album.id).first_or_404()
+        path = 'users/'  + str(current_user.id) + '/' + str(album.name) 
+        return render_template('photo.html',photo = photo, current_user=current_user, user = current_user, path = path, album_name=album_name)
     else:
         user = User.query.get_or_404(user_id)
-        album = user.albums.filter_by(name=album_name).first()
-        if album:
-            if album.public:
-                photo = Photo.query.filter_by(name = photo_name, album_id=album.id).first()
-                if photo:
-                    if photo.public:
-                        path = 'users/'  + str(user.id) + '/' + str(album.name) 
-                        return render_template('photo.html',photo = photo, current_user=current_user, user=user, path=path, description = photo.description, album_name=album_name)
-                    else:
-                        return '404', 404
-                else:
-                    return "Photos is private"
-            else: 
-                return '404', 404
-        else:
-            return '404', 404
-
-
+        album = user.albums.filter_by(name=album_name).first_or_404()
+        if album.public:
+            photo = Photo.query.filter_by(name = photo_name, album_id=album.id).first_or_404()
+            if photo.public:
+                path = 'users/'  + str(user.id) + '/' + str(album.name) 
+                return render_template('photo.html',photo = photo, current_user=current_user, user=user, path=path, description = photo.description, album_name=album_name)
+            else:
+                return current_app.login_manager.unauthorized()
+        else: 
+            return current_app.login_manager.unauthorized()
 
 
 @album_bp.route('/<int:user_id>/albums/<string:album_name>/<string:photo_name>', methods=['DELETE',])
 @login_required
 def delete_photo(user_id,album_name,photo_name):
     if current_user.id == user_id:
-        album = current_user.albums.filter_by(name=album_name).first()
-        if album:           
-            photo = album.photos.filter_by(name = photo_name).first()
-            db.session.delete(photo)
-            db.session.commit()
-            return '', 204
-        else:
-            return "404",404
+        album = current_user.albums.filter_by(name=album_name).first_or_404()
+        photo = album.photos.filter_by(name = photo_name).first()
+        db.session.delete(photo)
+        db.session.commit()
+        return jsonify({'status': 'deleted successfully'}), 204
     else:
-        return "404",404
+        return current_app.login_manager.unauthorized()
 
 
 @album_bp.route('/<int:user_id>/albums/<string:album_name>/<string:photo_name>', methods=['PUT',])
@@ -220,7 +184,8 @@ def update_photo(user_id,album_name,photo_name):
                 photo.likes = 0
         db.session.commit()
         return jsonify({"likes": photo.likes})
-
+    else:
+        return current_app.login_manager.unauthorized()
 
 
 @album_bp.route('/<int:user_id>/albums/<string:album_name>', methods=['PUT',])
@@ -233,7 +198,8 @@ def update_album(user_id, album_name):
             album.public = True
             db.session.commit()
             return jsonify({"status": "success"}), 204
-    return '404' , 404
+    else:
+        return current_app.login_manager.unauthorized()
 
 
 @album_bp.route('/<int:user_id>/albums/<string:album_name>/<string:photo_name>/comment', methods= ['POST',])
@@ -266,5 +232,3 @@ def add_comment(user_id, album_name, photo_name):
         db.session.commit()
         db.session.refresh(comment)
         return jsonify(comment.as_dict())
-
-    
